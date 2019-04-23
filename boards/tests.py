723d10515d9338b6990e55464b2f86799d959dc4
jho0078 <jho0078@gmail.com>
2019-04-23 04:43:33 +0000

    03_index && delete && update view test

--- boards/tests.py
+++ boards/tests.py
@@ -1,6 +1,7 @@
 # from django.test import TestCase
 from test_plus.test import TestCase
 from django.conf import settings
+from django.urls import reverse
 from .models import Board
 from .forms import BoardForm
 
@@ -33,12 +34,15 @@ class BoardModelTest(TestCase):
         data = {'title': '제목'}
         self.assertEqual(BoardForm(data).is_valid(), False)
         
+    
+        
+        
 # 3. View test
 # 기본 구조 : given - when - then
 class BoardViewTest(TestCase):
     # 공통적인 given 상황을 구성하기에 유용하다.
     def setUp(self):
-        user = self.make_user(username='test', password='qawsedrf!')
+        self.user = self.make_user(username='test', password='qawsedrf!')
         
     # create test 에서의 포인트는 form 을 제대로 주느냐이다. 가장 기본은 get_check_200
     def test_01_get_create(self):
@@ -69,6 +73,71 @@ class BoardViewTest(TestCase):
             self.assertContains(response, '')
             # form.is_valid() 를 통과하지 못해서 팅겨져 나옴.
             
+            
+    # detail 페이지가 제대로 출력되는지 확인
+    def test_05_detail_contains(self):
+        # given
+        # board = Board.objects.create(title='제목', content='내용', user=self.user)
+        board = Board.objects.create(title='제목', content='내용', user=self.user)
+        # when
+        self.get_check_200('boards:detail', board_pk=board.pk)
+        # then
+        self.assertResponseContains(board.title, html=False)
+        self.assertResponseContains(board.content, html=False)
+        
+    def test_06_detail_template(self):
+        board = Board.objects.create(title='제목', content='내용', user=self.user)
+        response =  self.get_check_200('boards:detail', board_pk=board.pk)
+        self.assertTemplateUsed(response, 'boards/detail.html')
+    
+    # 페이지가 잘 뜨는지
+    def test_07_get_index(self):
+        self.get_check_200('boards:index')
+        
+    # 페이지가 잘 뜨는지 + index.html 을 사용하고 있는지
+    def test_08_index_template(self):
+        response = self.get_check_200('boards:index')
+        self.assertTemplateUsed(response, 'boards/index.html')
+        
+    def test_09_index_queryset(self):
+        Board.objects.create(title='제목', content='내용', user=self.user)
+        Board.objects.create(title='제목', content='내용', user=self.user)
+        boards = Board.objects.order_by('-pk')
+        # when
+        response = self.get_check_200('boards:index')
+        # then
+        self.assertQuerysetEqual(response.context['boards'], map(repr, boards))
+        
+    # get 방식이기 때문에 오류가 발생
+    # def test_10_delete(self):
+    #     board = Board.objects.create(title='제목', content='내용', user=self.user)
+    #     with self.login(username='test', password='qawsedrf!'):
+    #         self.get_check_200('boards:delete', board_pk=board.pk)
+            
+    def test_11_delete_POST(self):
+        board = Board.objects.create(title='제목', content='내용', user=self.user)
+        with self.login(username='test', password='qawsedrf!'):
+            self.post('boards:delete', board_pk=board.pk)
+            self.assertEqual(Board.objects.count(), 0)
+            
+    def test_12_delete_redirect(self):
+        board = Board.objects.create(title='제목', content='내용', user=self.user)
+        with self.login(username='test', password='qawsedrf!'):
+            response = self.post('boards:delete', board_pk=board.pk)
+            # then
+            # 삭제 후 요청이 index 로 잘가는지
+            self.assertRedirects(response, reverse('boards:index'))
+            
+    def test_13_get_update(self):
+        board = Board.objects.create(title='제목', content='내용', user=self.user)
+        with self.login(username='test', password='qawsedrf!'):
+            response = self.get_check_200('boards:update', board.pk)
+            self.assertEqual(response.context['form'].instance.pk, board.pk)
+    
+    # 로그인 된 상태로 update로 들어갈 수 있는지
+    def test_14_get_update_login_required(self):
+        self.assertLoginRequired('boards:update', board_pk=1)
+        
 # 4. ModelForm
 # class BoardModelTest(TestCase):
             
